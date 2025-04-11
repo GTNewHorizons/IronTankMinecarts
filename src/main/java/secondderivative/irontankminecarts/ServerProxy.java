2e9c9a547e8e8b19f6cb2dbabb3f9fb9892fcc62
2ndDerivative <niclas@klugmann.de>
2025-04-11 23:10:48 +0200

    Add BS ServerProxy class

---
+++ src/main/java/secondderivative/irontankminecarts/ServerProxy.java
@@ -0,0 +1,8 @@
+package secondderivative.irontankminecarts;
+
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+
+public class ServerProxy extends CommonProxy {
+
+    public void postInit(FMLPostInitializationEvent event) {};
+}
